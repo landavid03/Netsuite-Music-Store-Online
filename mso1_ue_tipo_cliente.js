448f516778965fbaf9d53ff64d7ee87325b89add
DavidRazor <david.ramirez@razor-group.com>
2024-11-21 18:32:02 -0600

    feat: Add file 1

---
+++ mso1_ue_tipo_cliente.js
@@ -0,0 +1,11 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ */
+
+/* 
+1. Segmentación de Clientes:
+    • Crea un campo personalizado en el registro de Cliente llamado "Tipo de Cliente" (custentity_tipo_cliente).
+    • Este campo debe ser de tipo lista con las opciones: "Principiante", "Intermedio","Avanzado" y "Profesional".
+    • El campo debe ser obligatorio al crear un nuevo cliente. Nos ayudará a segmentar nuestras campañas de marketing.
+*/
